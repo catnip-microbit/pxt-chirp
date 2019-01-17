ca75870817d1e1bbc471670fb15d3e819f449adb
Rokas Šleinius <raveren@gmail.com>
2019-01-17 19:09:42 +0200

    Added light sensor fetch

--- chirp.ts
+++ chirp.ts
@@ -4,7 +4,7 @@ namespace chirp {
     //% blockSetVariable=humidity
     export function getHumidity(): number {
         pins.i2cWriteNumber(32, 0, NumberFormat.Int8LE, false)
-        basic.pause(100)
+        basic.pause(20)
         return pins.i2cReadNumber(32, NumberFormat.UInt16BE, true)
     }
 
@@ -13,11 +13,26 @@ namespace chirp {
     //% blockSetVariable=temperature
     export function getTemperature(): number {
         pins.i2cWriteNumber(32, 5, NumberFormat.Int8LE, false)
-        basic.pause(100)
+        basic.pause(20)
         return Math.round(pins.i2cReadNumber(32, NumberFormat.UInt16BE, true)/10)
     }
 
+    //% block="Chirp light value"
+    //% blockSetVariable=temperature
+    export function getLightLevel(): number {
+        pins.i2cWriteNumber(32, 3, NumberFormat.Int8BE, true)
+        
+        let buisy = false
+        do {
+            pins.i2cWriteNumber(32, 9, NumberFormat.Int8BE, true)
+            buisy = (0 != pins.i2cReadNumber(32, NumberFormat.Int8BE, true))
+        } while(buisy)
+            
+        pins.i2cWriteNumber(32, 4, NumberFormat.Int8BE, true)
+        return Math.round(pins.i2cReadNumber(32, NumberFormat.UInt16BE, true)/10)
+    }
 
+    
     //% block="Chirp is dry"
     export function isDry(): boolean {
         return getHumidity() < 300
